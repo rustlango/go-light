aa88c643a8aacefc63d57695d9c6ceb1363f9f13
Lemuel <alvorithmemail@gmail.com>
2022-08-17 09:36:19 +0200

    thre emain features of golang defined - v1

--- give-go-a-go/02-features-of-golang.go
+++ give-go-a-go/02-features-of-golang.go
@@ -1,6 +1,7 @@
 // 3 main Go Features
 
 // 1. Go is strong and statically typed language programming language
+// Types have to be specified at compile time - not at runtime
 
 // 2. Fast
 // Go programs have afats compilation time. They compile fast.

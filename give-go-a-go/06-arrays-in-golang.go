fdc554fcb1d469d35b71f8a0a75b7351f350076b
Lemuel <alvorithmemail@gmail.com>
2022-08-17 10:53:56 +0200

    created homogenous container or fruit arryaay

--- give-go-a-go/06-arrays-in-golang.go
+++ give-go-a-go/06-arrays-in-golang.go
@@ -7,7 +7,8 @@ package main
 import "fmt"
 
 func main() {
-
+	// one way of declaring an array
+	// size of array defined as 4. First element starts from 0
 	var fruits [4]string
 
 	fruits[0] = "Orange"
@@ -15,4 +16,10 @@ func main() {
 	fruits[2] = "Stawberry"
 	fruits[3] = "Litchi"
 	fmt.Print("Fruits array contains:", fruits)
+
+	// one way of declaring an array
+	// size of array defined as 4. First element starts from 0
+	crypto_balance_2:= [4]float32[12226271.3222, 3272726.3213, 422.322, 43777.4432211]
+	fmt.Print("List of crypto balances:", crypto_balance)
+}
 }

39821624598b739529553cfe35e7a86a42b70aba
Lemuel <alvorithmemail@gmail.com>
2022-08-17 10:42:30 +0200

    used const block and ipat to generate a counter - iota is a counter

--- give-go-a-go/05-constants-in-golang.go
+++ give-go-a-go/05-constants-in-golang.go
@@ -9,6 +9,21 @@ import "fmt"
 func main() {
 	const a int = 10
 	// cannot reassign a new value to the constant value a - compiler will cry
-	a = 20
+	// a = 20
 	fmt.Println("constant a equals", a)
+
+	// let us create a const block to contain multple constant values
+	// a ocnst blockc is nice because you it reduces the boilerplate
+	// of having to write out the const type multple times for each separate
+	// declaration if we were to do it the normal way
+	const (
+		// iota is a counter which starts from 0
+		b int = iota
+		c int = iota
+		d int = iota
+	)
+
+	fmt.Println("constant b equals", b)
+	fmt.Println("constant c equals", c)
+	fmt.Println("constant d equals", d)
 }

f739d6a7a1e02f890d4ef99a02c87bf1e05b348e
Lemuel <alvorithmemail@gmail.com>
2022-08-17 10:56:14 +0200

    used shorthand method for declaring arrays - method two

--- give-go-a-go/06-arrays-in-golang.go
+++ give-go-a-go/06-arrays-in-golang.go
@@ -15,11 +15,10 @@ func main() {
 	fruits[1] = "Apple"
 	fruits[2] = "Stawberry"
 	fruits[3] = "Litchi"
-	fmt.Print("Fruits array contains:", fruits)
+	fmt.Println("Fruits array contains:", fruits)
 
 	// one way of declaring an array
 	// size of array defined as 4. First element starts from 0
-	crypto_balance_2:= [4]float32[12226271.3222, 3272726.3213, 422.322, 43777.4432211]
-	fmt.Print("List of crypto balances:", crypto_balance)
-}
+	crypto_balance := [4]float32{12226271.3222, 3272726.3213, 422.322, 43777.4432211}
+	fmt.Println("List of crypto balances:", crypto_balance)
 }

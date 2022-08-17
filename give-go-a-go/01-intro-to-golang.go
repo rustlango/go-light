a635be348083946ca5c11588314554ad99142ef1
Lemuel <alvorithmemail@gmail.com>
2022-08-17 09:35:21 +0200

    thre emain features of golang defined

--- give-go-a-go/01-intro-to-golang.go
+++ give-go-a-go/01-intro-to-golang.go
@@ -1,7 +1,7 @@
 // language is called golang
 // hello world program
 // go run is a command to run a golang program so but no binary or executab
-// f=go build generates a binary
+// go build generates a binary
 package main
 
 import "fmt"
---
+++ give-go-a-go/02-features-of-golang.go
@@ -0,0 +1,10 @@
+// 3 main Go Features
+
+// 1. Go is strong and statically typed language programming language
+
+// 2. Fast
+// Go programs have afats compilation time. They compile fast.
+
+//3. Go has a Garabage collector bt instead of going the error-prone route for mnaually
+// conducting memory management - it does it uatomatically.
+// It has an automatic Garabage collection

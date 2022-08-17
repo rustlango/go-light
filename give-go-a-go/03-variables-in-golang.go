e65cd261c1e53419e1942ca9452cd843d8e0f38c
Lemuel <alvorithmemail@gmail.com>
2022-08-17 09:53:42 +0200

    variable declaration methods defined - printed using pritnf method

--- give-go-a-go/03-variables-in-golang.go
+++ give-go-a-go/03-variables-in-golang.go
@@ -11,9 +11,17 @@ import "fmt"
 
 func main() {
 	// how to create a variable in golang
+	// explicit variable decalration
 	var a int = 10
 	var b int = 20
+	// implicit variable decalration for type inference
+	c := 100
 	// Println() is a  method from the standard fmt library to
 	// print to the screen
 	fmt.Println("Hello, World")
+	// %v returns the value and %T returns the type
+	fmt.Printf("%v, %T %()", c, c, a+b+c)
+	// retruns : 100, int %!{(int=130)}
+	// compiler has the ability infer the type for you though implicit variable
+	// variable declaration is not always encouraged
 }

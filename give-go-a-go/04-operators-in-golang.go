9ba356fe6ed43d2387aba247706c9cdb411944ea
Lemuel <alvorithmemail@gmail.com>
2022-08-17 10:33:07 +0200

    made println statements more readable for outputs

--- give-go-a-go/04-operators-in-golang.go
+++ give-go-a-go/04-operators-in-golang.go
@@ -14,19 +14,19 @@ func main() {
 
 	var a int = 10
 	var b int = 20
-	fmt.Println(a / b)  // will be zero because the datat types are integers not float
-	fmt.Println(a > b)  // false
-	fmt.Println(a == b) //  false: 10 is ot equal to b
-	fmt.Println(a != b) // true: a is not equal to b
+	fmt.Println("what is int divided by d int?", a/b) // will be zero because the datat types are integers not float
+	fmt.Println("is a greater than b?", a > b)        // false
+	fmt.Println("is a equal to b?", a == b)           //  false: 10 is ot equal to b
+	fmt.Println("is a not equal to b?", a != b)       // true: a is not equal to b
 	var c float32 = 10
 	var d float32 = 20
-	fmt.Println(c / d)  // returns 0.5 because flota32 type not ineteger
-	fmt.Println(c < d)  // true
-	fmt.Println(c == d) //  false: c is not equal to d
-	fmt.Println(c != d) // true: c is not equal to d
+	fmt.Println("what is c float32 / d float32?", c/d) // returns 0.5 because flota32 type not ineteger
+	fmt.Println("is c less than d?", c < d)            // true
+	fmt.Println("is c = to d?", c == d)                //  false: c is not equal to d
+	fmt.Println("is c not = to d?", c != d)            // true: c is not equal to d
 	// && is an AND logical operator and || is the OR operator
 	var e bool = false
 	var f bool = true
-	fmt.Println(e && f)                 // returns false
-	fmt.Println("e or f is %d", e || f) // returns true
+	fmt.Println("e AND f", e && f) // returns false
+	fmt.Println("e OR f", e || f)  // returns true
 }

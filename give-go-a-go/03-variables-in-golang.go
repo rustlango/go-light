ca39a93bb4d3b36b8df47708ffd1ea05406b3b7b
Lemuel <alvorithmemail@gmail.com>
2022-08-17 09:43:35 +0200

    created a variable in golang

---
+++ give-go-a-go/03-variables-in-golang.go
@@ -0,0 +1,18 @@
+// Variables in golang
+
+// Data/Values can be stored in temporary storage spacs called variables
+// in rust they are called bindings
+
+// main papckage is the entry point of the golang program
+package main
+
+// fmt is the standard library f golang
+import "fmt"
+
+func main() {
+	// how to create a variable in golang
+	var a int = 10
+	// Println() is a  method from the standard fmt library to
+	// print to the screen
+	fmt.Println("Hello, World")
+}

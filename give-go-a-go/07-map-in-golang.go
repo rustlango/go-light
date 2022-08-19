bb031b50848803bf5a99f782682f9285c9100e42
Lemuel Solomon <49029145+alvorithm@users.noreply.github.com>
2022-08-19 10:06:21 +0200

    Update 07-map-in-golang.go
    Notes on key value types annotated. Fix: spelling error on comments.

--- give-go-a-go/07-map-in-golang.go
+++ give-go-a-go/07-map-in-golang.go
@@ -6,12 +6,13 @@ import "fmt"
 
 func main() {
 	// This is one way of declaring or creating a map in go using a map literal
+	// keys are string type and values are float32 type
 	crypto_wallet_balances := map[string]float32{
 		"Etheurem": 23.77792,
 		"Bitcoin":  212.288004322,
 		"Atom":     123.3221,
 		"DOT":      2399.999,
 	}
-	// it will print the map KV properties i alphabetical order
+	// it will print the map KV properties in alphabetical order
 	fmt.Println("Crypto balances in wallet are a follows: ", crypto_wallet_balances)
 }

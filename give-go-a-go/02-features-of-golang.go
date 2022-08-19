b3ec4d52ba5cf0e6320f4fde5a73ce932b0dbe79
Lemuel Solomon <49029145+alvorithm@users.noreply.github.com>
2022-08-19 09:52:31 +0200

    Update 02-features-of-golang.go
    Updated notes on golang

--- give-go-a-go/02-features-of-golang.go
+++ give-go-a-go/02-features-of-golang.go
@@ -4,10 +4,11 @@
 // Types have to be specified at compile time - not at runtime
 
 // 2. Fast
-// Go programs have afats compilation time. They compile fast.
+// Go programs have a fast compilation time. They compile fast.
 
-//3. Go has a Garabage collector bt instead of going the error-prone route for mnaually
-// conducting memory management - it does it uatomatically.
+//3. Go has a Garabage collector but instead of going the error-prone route of manually
+// conducting memory management - it does it automatically but it is not as bloated as a 
+// dynamic typed language hence why it is perfromant.
 // It has an automatic Garabage collection
 // go compiler automatically alocates and deallocates memory of data after it's use
 // or lifetime (as rustaceans would call it)
